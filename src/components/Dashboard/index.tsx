b8423ef9bfe06e92923290816d3d99a4c0d8230b
Laura L. F <lauralf.eso@gmail.com>
2023-07-10 08:39:24 -0300

    first

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,14 @@
+import { Summary } from "../Summary";
+import { TransactionTable } from "../TransactionTable";
+import { Container } from "./style";
+
+export function Dashboard(){
+    return (
+        <>
+            <Container>
+                 <Summary />
+                 <TransactionTable />
+            </Container>
+        </>
+    )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import {createServer, Model} from 'miragejs'
+import { App } from './App'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+createServer({
+    models: {
+        transaction: Model,
+    },
+    seeds(server){
+        server.db.loadData({
+            transactions: [
+                {
+                    id: 1,
+                    title: 'Desenvolvimento de Sites',
+                    type: 'deposit',
+                    category: 'Atendimento',
+                    amount: 2500,
+                    createdAt: new Date('2023-04-06 10:00:00'),
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+                },
+                {
+                    id: 2,
+                    title: 'Aluguel',
+                    type: 'withdraw',
+                    category: 'Atendimento',
+                    amount: 1000,
+                    createdAt: new Date('2023-04-07 10:00:00'),
+
+                }
+            ]
+        })
+    },
+    routes() {
+        this.namespace = 'api';
+
+        this.get('/transactions', ()=>{
+            return this.schema.all('transaction')
+        })
+
+        this.post('/transactions', (schema, request) => {
+            const data = JSON.parse(request.requestBody);
+            return schema.create('transaction', data)
+        })
+    },
+})
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
